c5c2808f7da3b20a5919e33a283ae20ceea6a16e
ehanson94 <ehanson94@gmail.com>
2019-11-22 14:09:43 -0500

    finished writing functions in main.py

--- main.py
+++ main.py
@@ -6,32 +6,53 @@ __author__ = "???"
 
 def add(x, y):
     """Add two integers. Handles negative values."""
-    # your code here
-    return
+    return (x) + (y)
 
 
 def multiply(x, y):
     """Multiply x with y. Handles negative values of x or y."""
-    # your code here
-    return
+    mult = 0
+    if x < 0 and y > 0:
+        for i in range(y):
+            mult = add(mult, x)
+    elif x > 0 and y < 0:
+        for i in range(x):
+            mult = add(mult, y)
+    elif x < 0 and y < 0:
+        for i in range(-y):
+            mult = add(mult, -x)
+    else:
+        for i in range(x):
+            mult = add(mult, y)
+
+    return mult
 
 
 def power(x, n):
     """Raise x to power n, where n >= 0"""
-    # your code here
-    return
+    power = 1
+    for i in range(n):
+        power = multiply(power, x)
+    return power
 
 
 def factorial(x):
     """Compute factorial of x, where x > 0"""
-    # your code here
-    return
+    factorial = 1
+    if x == 0:
+        return 1
+    else:
+        for i in range(1, x + 1):
+            factorial = multiply(factorial, i)
+    return factorial
 
 
 def fibonacci(n):
     """Compute the nth term of fibonacci sequence"""
-    # your code here
-    return
+    fibonacci = 1
+    for i in range(n + 1):
+        fibonacci = add(fibonacci, i)
+    return fibonacci
 
 
 if __name__ == '__main__':
